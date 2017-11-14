b48c42278fb6c395dfbbe4d443551ce57b36467d
Ted-Koomen <koomen.ted@gmail.com>
2017-11-14 13:56:07 -0500

    add content component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavHeader from './components/Navbar/Navbar'
-
+import Content from './components/Content/Content'
+import Contact from './components/Contact/Contact'
 class App extends Component {
   render() {
     return (
-      <NavHeader/>
+      <div className="container">
+        <NavHeader/>
+        <Content />
+        <Contact />
+      </div>
     )
   }
 }
