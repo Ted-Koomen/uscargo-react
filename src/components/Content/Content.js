d978d6579ba903c5d266c0b71386c1c292e94cee
Ted-Koomen <koomen.ted@gmail.com>
2017-11-27 09:55:44 -0500

    edit styling

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -2,19 +2,14 @@ import React from 'react'
 import './Content.css'
 const Content = (props) => {
   return (
-    <div className="container">
-      <div className="row my-4">
-        <div className="col-lg-8">
-          
-          <img className="img-responsive main-image" height="400" width="600" src="http://socalinsurance.com/wp-content/uploads/2017/07/trucking-insurance.png" className="img-fluid rounded"></img>
-        </div>
-        <div className="col-lg-4">
-          <p>US Cargo puts logic in logistics. We have the most competitive rates, and the highest level of customer service needed for your most valuable of shipments. Whether you are a small business trying to move a little cargo, or a large corporation looking to shop large amounts frequently, we work with the right people to suit your needs</p>
-        </div>
-      </div>
-      <div className="card text-white bg-secondary my-4 text-center">
-        <div className="card-body">
-          <p className="text-white m-0">US Cargo puts the logic in logistics!</p>
+    <div className="middle-fixed-1 container">
+      <div className="row">
+        <div className="panel-pane pane-node">
+          <div className="section about-rto">
+            <div className="container">
+              <h1 className font-weight="100">Learn about US Cargo. Make a <span class="accent-text">smart</span> decision.</h1>
+            </div>
+          </div>
         </div>
       </div>
     </div>
