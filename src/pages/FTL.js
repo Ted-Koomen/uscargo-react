a99ad1a4e16fce9e135b31ca90693195cc8c8937
Ted-Koomen <koomen.ted@gmail.com>
2017-11-28 13:10:05 -0500

    add images

--- src/pages/FTL.js
+++ src/pages/FTL.js
@@ -1,10 +1,20 @@
 import React from 'react'
-
+import './FTL.css'
 const FTL = (props) => {
     return (
-        <div className="section about-rto">
-            <div className="container">
-                <h1 className="text-center">What is FTL?</h1>
+        <div>
+            <div className="top" >
+                <div className="panel-pane pane-custom pane-1">
+                    <div className="ftl">
+
+                    </div>
+
+                </div>
+            </div >
+            <div className="section about-rto">
+                <div className="container">
+                    <h1 className="text-center">What is FTL transport?</h1>
+                </div>
             </div>
         </div>
     )
--- src/pages/Sea.js
+++ src/pages/Sea.js
@@ -1,10 +1,20 @@
 import React from 'react'
-
+import './Sea.css'
 const Sea = (props) => {
     return (
-        <div className="section about-rto">
-            <div className="container">
-                <h1 className="text-center">What is Sea Transport?</h1>
+        <div>
+            <div className="top" >
+                <div className="panel-pane pane-custom pane-1">
+                    <div className="container-ship">
+
+                    </div>
+
+                </div>
+            </div >  
+            <div className="section about-rto">
+                <div className="container">
+                    <h1 className="text-center">What is Sea Freight?</h1>
+                </div>
             </div>
         </div>
     )
