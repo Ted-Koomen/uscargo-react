5c67e52ae51cb96535c6ec661dcb5750fcf84d88
Ted-Koomen <koomen.ted@gmail.com>
2017-11-29 11:36:43 -0500

    add email a tag

--- src/pages/ContactPage/ContactPage.js
+++ src/pages/ContactPage/ContactPage.js
@@ -7,22 +7,22 @@ const ContactPage = (props) => {
             <h1 className="text-center">Contact</h1>
             <div id="felix">
                 <h3 className="page-header">Felix <small>President & CEO</small></h3>
-                <p>email: fgicz@uscargo.us</p>
+                <p>email: <a href="mailto:fgicz@uscargo.us" target="_top">fgicz@uscargo.us</a></p>
                 <p>phone: 123-456-7890</p>
             </div>
             <div id="dan">
                 <h3 className="page-header">Dan <small>Account Manager & Partner</small></h3>
-                <p>email: dan@uscargo.us</p>
+                <p>email: <a href="mailto:dan@uscargo.us" target="_top">dan@uscargo.us</a></p>
                 <p>phone: 123-456-7890</p>
             </div>
             <div id="matt">
                 <h3 className="page-header">Matt <small>Account Manger</small></h3>
-                <p>email: matt@uscargo.us</p>
+                <p>email: <a href="mailto:matt@uscargo.us" target="_top">matt@uscargo.us</a></p>
                 <p>phone: 123-456-7890</p>
             </div>
             <div id="teddy">
                 <h3 className="page-header">Teddy <small>Software Engineer</small></h3> 
-                <p>email: ted@uscargo.us</p>
+                <p>email: <a href="mailto:ted@uscargo.us" target="_top">ted@uscargo.us</a></p>
                 <p>phone: 123-456-7890</p>
             </div>
         </div>
